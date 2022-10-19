05333ac5c5b222c8b7b91f3501f3a25d0ed26500
hieunguyen <hieun140201@gmail.com>
2022-10-19 13:18:33 +0700

    bai tap printf chu nhat 16-10-2022

---
+++ baiprintf1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    char string[100];
+    printf("Name : Alexandra Abramov\n", string);
+    printf("DOB : July 14, 1975\n", string);
+    printf("Mobile : 99-9999999999", string);
+    return 0;
+}
---
+++ baiprintf2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char string[] = 'H';//
+    // char b = 'U';
+    // char c = 'Y';
+    printf("reverse cua %c,%c,%c la %c,%c,%c\n", a,b,c, c,b,a);
+    
+    // char string[100];
+    // printf("nguyen van hieu\n", string);
+    // printf("nguyen van hieu",strrev(string));
+    return 0;
+}
