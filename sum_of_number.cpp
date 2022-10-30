0f8c5c0aeda4366dedc48b51c90a55672d35eae8
hieunguyen <hieun140201@gmail.com>
2022-10-30 11:57:36 +0700

    Sum of number

---
+++ sum_of_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+   int a=7 , b=5 , c=1;
+   float d=0.5 , e=9.45 , f=0.55;
+   printf("7 + 5 = %d\n",a+b);
+   printf("0.5 + 0.5 =%.0f\n",d+d);
+   printf("9.45 + 0.55 =%.0f\n",e+f);
+   printf("1 + 0.5 = %.1f\n",c+d);
+
+ 
+   return(0);
+}
