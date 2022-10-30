2a785f050f2753e5416658f7d95d1b37b78e2b87
hieunguyen <hieun140201@gmail.com>
2022-10-30 09:23:58 +0700

    update

--- baiprintf2.cpp
+++ baiprintf2.cpp
@@ -3,10 +3,13 @@
 
 int main()
 {
-    char string[] = 'H';//
+     //string doi_ten = "H,U,Y";
+     char a = 'H', b = 'U', c = 'Y'; //khoi tao va khai bao bien bang 1 dong
+     printf("ten dao nguoc cua %c,%c,%c la %c,%c,%c\n", a,b,c, c,b,a);
+     //char a = 'H'
     // char b = 'U';
     // char c = 'Y';
-    printf("reverse cua %c,%c,%c la %c,%c,%c\n", a,b,c, c,b,a);
+    //printf("reverse cua %c,%c,%c la %c,%c,%c\n", a,b,c, c,b,a);
     
     // char string[100];
     // printf("nguyen van hieu\n", string);
---
+++ baiprintf3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdbool.h>
+//in tat ca cac kieu bien trong c
+int main()
+{ 
+    char string[100];
+    int a = 20;
+    float b = 1.78 ,c = 55.5;
+    bool d = true , e = false;
+    printf("full name: Nguyen Van Hieu\n",string);
+    printf ("toi nam nay %d tuoi\n",a);
+    printf("chieu cao la %f va can nang la %f\n",b,c);
+    printf("true,false",d,e);
+    return 0;
+}
