5990b83730325d8ab1f419235245f14acbf7d9d1
hieunguyen <hieun140201@gmail.com>
2022-10-30 09:46:16 +0700

    update

--- baiprintf1.cpp
+++ baiprintf1.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    char string[100];
-    printf("Name : Alexandra Abramov\n", string);
-    printf("DOB : July 14, 1975\n", string);
-    printf("Mobile : 99-9999999999", string);
+    
+    printf("Name : Alexandra Abramov\n");
+    printf("DOB : July 14, 1975\n");
+    printf("Mobile : 99-9999999999");
     return 0;
 }
--- baiprintf3.cpp
+++ baiprintf3.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <stdbool.h>
+
 //in tat ca cac kieu bien trong c
 int main()
 { 
-    char string[100];
+   
     int a = 20;
     float b = 1.78 ,c = 55.5;
     bool d = true , e = false;
-    printf("full name: Nguyen Van Hieu\n",string);
+    printf("full name: Nguyen Van Hieu\n");
     printf ("toi nam nay %d tuoi\n",a);
-    printf("chieu cao la %f va can nang la %f\n",b,c);
+    printf("chieu cao la %.2f va can nang la %.2f\n",b,c);
     printf("true,false",d,e);
     return 0;
 }
