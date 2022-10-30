1fe1c312a0add1013f93fb0084b23d4bb3777997
hieunguyen <hieun140201@gmail.com>
2022-10-30 11:06:00 +0700

    Compute perimeter and area of circle

---
+++ Compute_premier_and_area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+    int a=6 , b=2;
+    float c= 3.14;
+    printf("Perimeter of the Circle = %f inches\n",a*b*c);
+    printf("Area of the Circle = %f square inches",a*a*c);
+    return 0;
+}
