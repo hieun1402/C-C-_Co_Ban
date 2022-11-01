a815af8c7ca8e9521a2a98726f447bfa7a1404d9
hieunguyen <hieun140201@gmail.com>
2022-11-01 23:08:55 +0700

    khai bao va khoi tao

--- baiprintf3.cpp
+++ baiprintf3.cpp
@@ -5,9 +5,12 @@
 int main()
 { 
    
-    int a = 20;
-    float b = 1.78 ,c = 55.5;
-    bool d = true , e = false;
+    int a; //khai bao a la kieu du lieu int
+    a=20;  //khoi tao la gan cho a 1 gia tri 20
+    float b ,c;
+    b=1.78 , c=55.5;
+    bool d , e;
+    d=true , e=false;
     printf("full name: Nguyen Van Hieu\n");
     printf ("toi nam nay %d tuoi\n",a);
     printf("chieu cao la %.2f va can nang la %.2f\n",b,c);
