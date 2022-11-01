937d5ba79d03c3062504169308e199199c4463c2
hieunguyen <hieun140201@gmail.com>
2022-11-01 23:02:47 +0700

    up date

--- sum_of_number.cpp
+++ sum_of_number.cpp
@@ -4,9 +4,9 @@ int main()
 {
    int a=7 , b=5 , c=1;
    float d=0.5 , e=9.45 , f=0.55;
-   printf("7 + 5 = %d\n",a+b);
-   printf("0.5 + 0.5 =%.0f\n",d+d);
-   printf("9.45 + 0.55 =%.0f\n",e+f);
-   printf("1 + 0.5 = %.1f\n",c+d);
+   printf("%d + %d = %d\n",a,b,a+b);
+   printf("%.1f + %.1f =%.0f\n",d,d,d+d);
+   printf("%.2f + %.2f =%.0f\n",e,f,e+f);
+   printf("%d + %.1f = %.1f\n",c,d,c+d);
    return(0);
 }
