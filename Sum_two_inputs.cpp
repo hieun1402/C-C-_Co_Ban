fbb3c1a0a737b99d0195ab965ae0106c790b24c8
hieunguyen <hieun140201@gmail.com>
2022-11-01 23:17:54 +0700

    Sum two inputs

---
+++ Sum_two_inputs.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    int a , b;
+    printf("First number: \n");
+    scanf("%d",&a);
+    printf("Second number: \n");
+    scanf("%d",&b);
+    printf("sum= %d",a+b);
+    return 0;
+}
