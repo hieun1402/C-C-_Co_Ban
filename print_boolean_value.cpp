f12f7d4d7b29dd2a9912b7b2afade835d3bd5886
hieunguyen <hieun140201@gmail.com>
2022-11-06 15:00:03 +0700

    print boolean value

---
+++ print_boolean_value.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    bool gender = true ;
+    if(gender == true){
+        printf("My gender is male");
+    }else{
+        printf("My gender is female");
+    }
+    return 0;
+}
