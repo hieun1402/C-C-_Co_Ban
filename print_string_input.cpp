9616c4c3ed9e27f37319e6cba45a89ad45043c53
hieunguyen <hieun140201@gmail.com>
2022-11-06 14:40:35 +0700

    print string input

---
+++ print_string_input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+int main()
+{
+    int a ;
+    char string[100] ;
+    printf("my name is : \n");
+    scanf("%s",&string);
+    printf("my year old : \n");
+    scanf("%d",&a);
+    //printf("my year old is %d",a);
+    printf("My name is %s and I'm %d year old",string,a);
+    return 0;
+}
