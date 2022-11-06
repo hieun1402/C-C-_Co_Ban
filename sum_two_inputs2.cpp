db4c362c448fca52f7afe6df06ca682c5efec7e9
hieunguyen <hieun140201@gmail.com>
2022-11-06 14:12:57 +0700

    input 2 so thap phan

---
+++ sum_two_inputs2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    float a , b ;
+    printf("first number : \n");
+    scanf("%f",&a);
+    printf("second number : \n");
+    scanf("%f",&b);
+    printf("sum of float number : %.0f",a+b);
+    return 0;
+}
