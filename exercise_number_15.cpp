5bf40fba825cfda5c25e5d14a80474a757452f17
hieunguyen <hieun140201@gmail.com>
2022-11-11 22:37:07 +0700

    the distance between the two points.

---
+++ exercise_number_15.cpp
@@ -0,0 +1,15 @@
+#include<iostream> //khai bao thu vien
+#include<math.h>
+int main(){
+    float x1 , y1 , x2 , y2 , dis;
+    printf("Input x1: ");
+    scanf("%f",&x1);
+    printf("Input y1: ");
+    scanf("%f",&y1);
+    printf("Input x2: ");
+    scanf("%f",&x2);
+    printf("Input y2: ");
+    scanf("%f",&y2);
+    printf("Distance between the said points: %.4f",sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))));
+    return 0;
+}
