0c1b88f88933ba34160bf220c3574eb0d74ed1fb
hieunguyen <hieun140201@gmail.com>
2022-11-19 22:18:47 +0700

    3 numbers in a line, starting from 1 and print n lines

---
+++ exercise_41.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+int  c=10 , b=5 , i=0 , j=1 , f;
+char a;
+printf("Input number of lines: ");
+scanf("%c",&a);
+for(f=0 ; f<b ; f++){
+    for (i=0 ; i<c ; i++)
+    {
+        printf("%c ",a);
+        
+    }
+    // i = 0;
+    printf("\n");
+}
+    return 0;
+}
