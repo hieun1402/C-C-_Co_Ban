0a00dfad0741a52f2d2a222acf214f506c4b3c69
hieunguyen <hieun140201@gmail.com>
2023-01-07 16:39:32 +0700

    prints the squares and cubes of the numbers from 0 to 20

---
+++ exer_76.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(){
+int i , n , m;
+printf("Number Square Cube: \n");
+printf("======================\n");
+for(i=0 ; i<21 ; i++){
+    // printf("%d  ",i);
+    // n = i * i;
+    // printf("%d  ",n);
+    // m = i*i*i;
+    // printf("%d  ",m);
+    // printf("\n");
+    printf("%d\t%d\t%d\n  ",i,i*i,i*i*i);
+}
+    return 0;
+}
