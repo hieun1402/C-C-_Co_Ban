fd7c60e5371942509770088bbcc39a0e71006d5d
hieunguyen <hieun140201@gmail.com>
2023-01-07 16:51:43 +0700

    exer78

---
+++ exer_78.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main()
+{
+    int x = 10;
+    printf("Predecrementing:\n");
+    printf("x   = %d\n", x);
+    printf("x-- = %d\n", x--);
+    printf("x   = %d\n\n", x);
+    
+    x = 10;
+    printf("Postdecrementing:\n");
+    printf("  x = %d\n", x);
+    printf("--x = %d\n", --x);
+    printf("  x = %d\n", x);
+    
+    return 0;
+}
